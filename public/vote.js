462c21e598dfff2287f0757628ccb15d615c3e9b
Steve <styvwheeler@gmail.com>
2017-03-18 14:55:18 +0100

    Fixed twitter bug, cannot vote more than once, removed random feature

--- public/vote.js
+++ public/vote.js
@@ -3,25 +3,29 @@ $(document).ready(function() {
   $('.answer-button').on('click', function () {
     let _id = $(this).parent().attr("id");
     let question_vote = $(this).attr('id') + "_votes";
-
+    let answer = $(this).attr('id');
+    let user = $('#user').text()
+    console.log(user);
     fetch('submitVote', {
       method: 'put',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         '_id': _id,
-        'questionToUpdate': question_vote
+        'questionToUpdate': question_vote,
+        'user': user,
+        'answer': answer
       })
     }).then(res => {
-      if (res.ok) return res.json()
-    }) .then(data => {
+      if (res.ok) {
         window.location.reload()
-      })
-
+      } else {
+        alert("You have already voted for that")
+      }
+    })
   })
 
-  $('#tweetIt').click(function(){
-    console.log('click');
-    let tweetText = $('#tweetText').html();
+  $('.tweetIt').click(function(){
+    let tweetText = $(this).children().text();
     window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweetText));
   })
 
