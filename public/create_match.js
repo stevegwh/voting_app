4b11c6b98e67a01d4e9df7c8c8daa5fe0ce56956
Steve <styvwheeler@gmail.com>
2017-03-19 00:02:41 +0100

    Fixed create match UI

--- public/create_match.js
+++ public/create_match.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
   $('#add-option').on('click', function(){
-    let count = $("#options").children().length + 1;
+    let options = $('#options').children().length;
+    let count = options + 1;
+    console.log($('#options').children())
+    if(options < 5) {
 
-    $('#options').append('\
-    <div>\
-      <input class="steve-col" type="text" name="answer'+ count +'" placeholder="Option">\
-      <div class=" btn btn-danger btn-xs remove-option">X</div>\
-    </div>');
+
+      $('#options').append('\
+      <div>\
+        <input class="steve-col" type="text" name="answer'+ count +'" placeholder="Option">\
+        <div class=" btn btn-danger btn-xs remove-option">X</div>\
+      </div>');
+    } else {
+      alert("Sorry, you cannot have more than " + options + " options");
+    }
   })
 
-  $('.remove-option').on('click', function(){
+  $(document).on('click', '.remove-option', function() {
+    let options = $('#options').children().length;
+    let count = 1;
+    $(this).parent().remove();
+    $('input', $('#options')).each(function () {
+        $(this).attr("name", "answer" + count)
+        count++;
+    });
 
-  })
+  });
 })
